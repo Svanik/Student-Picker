58deb61473e178db0fce1bc2def02887fd883472
Svanik <34766427+Svanik@users.noreply.github.com>
2018-01-20 21:42:11 -0500

    Inital Commit Basic Randomizier for picking kids in class, wil be improved

---
+++ randomizer.py
@@ -0,0 +1,12 @@
+import random
+
+
+students = ["Halle Conaghan", 'Svanik Dani', 'Daniella Donohue',
+            'Christian Fusco', 'Marilise DeBuck', 'Lennox Lamar',
+            'Christopher Mayo', 'Jacob Naddelman', 'Isabel Piantini',
+            'Youcef Soltani', 'Rebeca Vargas', 'Tyler Wasilewski']
+
+x = len(students)
+print(x)
+pick = random.choice(students)
+print(pick)
