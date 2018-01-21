ce3a72c1d6a858d1bb23de2976fc2dce8ac2004e
Svanik <34766427+Svanik@users.noreply.github.com>
2018-01-21 01:03:05 -0500

    Custom student adeed gona need to be rmoved

--- randomizer.py
+++ randomizer.py
@@ -1,3 +1,5 @@
+"""Random picker thaat may be used in classes made for my bio class."""
+
 import random
 
 
@@ -5,8 +7,18 @@ students = ["Halle Conaghan", 'Svanik Dani', 'Daniella Donohue',
             'Christian Fusco', 'Marilise DeBuck', 'Lennox Lamar',
             'Christopher Mayo', 'Jacob Naddelman', 'Isabel Piantini',
             'Youcef Soltani', 'Rebeca Vargas', 'Tyler Wasilewski']
+pickedstudents = []
 
-x = len(students)
-print(x)
+studlen = len(students)
+print(studlen)
+# just to pick one student
 pick = random.choice(students)
 print(pick)
+# for test seating
+choicenum = 1
+while choicenum < (studlen + 1):
+    pick = random.choice(students)
+    students.remove(pick)
+    pickedstudents.append(pick)
+    print(f'#{choicenum} {pick}')
+    choicenum = choicenum + 1
