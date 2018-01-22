ed2f670ee3ba962f5f76533d7b2db0255c071c82
Svanik <34766427+Svanik@users.noreply.github.com>
2018-01-21 19:11:30 -0500

    Completed all desired featered through command line

--- randomizer.py
+++ randomizer.py
@@ -1,6 +1,65 @@
 """Random picker thaat may be used in classes made for my bio class."""
-
 import random
+
+
+def pickonestudent():
+    pick = random.choice(students)
+    print(pick)
+
+
+def pickxstudents():
+    numofstud = input("Enter Num of Students to Pick: ")
+    if numofstud.isnumeric():
+        numofstud = int(numofstud)
+        choicenum = 1
+        while choicenum < (numofstud + 1):
+            pick = random.choice(students)
+            students.remove(pick)
+            pickedstudents.append(pick)
+            print(f'#{choicenum} {pick}')
+            choicenum = choicenum + 1
+    else:
+        print("You did not enter a number try again. ")
+        pickxstudents()
+
+
+def pickallstudents():
+    studlen = len(students)
+    choicenum = 1
+    while choicenum < (studlen + 1):
+        pick = random.choice(students)
+        students.remove(pick)
+        pickedstudents.append(pick)
+        print(f'#{choicenum} {pick}')
+        choicenum = choicenum + 1
+
+
+def goagain():
+    while True:
+        goagain = input("Would you like to pick again. (y/n)")
+        if goagain == 'y' or goagain == 'Y':
+            x = 1
+            break
+        elif goagain == 'n' or goagain == 'N':
+            x = 0
+            break
+        else:
+            print('Invalid Input, Please retry')
+    return x
+
+
+def pickthistodo():
+    dothis = input('''Please enter the number that correseponds with the option
+                    1) Pick 1 students
+                    2) Pick 3x students
+                    3) Pick all studnets
+                    Enter Choice: ''')
+    if dothis == '1' or dothis == '2' or dothis == '3':
+        dothis = int(dothis)
+        return dothis
+    else:
+        print("Your input is not valid pleace input 1, 2, or 3")
+        pickthistodo()
 
 
 students = ["Halle Conaghan", 'Svanik Dani', 'Daniella Donohue',
@@ -9,16 +68,32 @@ students = ["Halle Conaghan", 'Svanik Dani', 'Daniella Donohue',
             'Youcef Soltani', 'Rebeca Vargas', 'Tyler Wasilewski']
 pickedstudents = []
 
-studlen = len(students)
-print(studlen)
-# just to pick one student
-pick = random.choice(students)
-print(pick)
-# for test seating
-choicenum = 1
-while choicenum < (studlen + 1):
-    pick = random.choice(students)
-    students.remove(pick)
-    pickedstudents.append(pick)
-    print(f'#{choicenum} {pick}')
-    choicenum = choicenum + 1
+x = 1
+while x == 1:
+    donow = pickthistodo()
+    if donow == 1:
+        pickonestudent()
+        students = ["Halle Conaghan", 'Svanik Dani', 'Daniella Donohue',
+                    'Christian Fusco', 'Marilise DeBuck', 'Lennox Lamar',
+                    'Christopher Mayo', 'Jacob Naddelman', 'Isabel Piantini',
+                    'Youcef Soltani', 'Rebeca Vargas', 'Tyler Wasilewski']
+        pickedstudents = []
+        x = goagain()
+    elif donow == 2:
+        pickxstudents()
+        students = ["Halle Conaghan", 'Svanik Dani', 'Daniella Donohue',
+                    'Christian Fusco', 'Marilise DeBuck', 'Lennox Lamar',
+                    'Christopher Mayo', 'Jacob Naddelman', 'Isabel Piantini',
+                    'Youcef Soltani', 'Rebeca Vargas', 'Tyler Wasilewski']
+        pickedstudents = []
+        x = goagain()
+    elif donow == 3:
+        pickallstudents()
+        students = ["Halle Conaghan", 'Svanik Dani', 'Daniella Donohue',
+                    'Christian Fusco', 'Marilise DeBuck', 'Lennox Lamar',
+                    'Christopher Mayo', 'Jacob Naddelman', 'Isabel Piantini',
+                    'Youcef Soltani', 'Rebeca Vargas', 'Tyler Wasilewski']
+        pickedstudents = []
+        x = goagain()
+    else:
+        x = 1
